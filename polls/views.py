ae780b29c96175add81f198c7e999f120191c5a5
nylee-lecture <nylee.class@gmail.com>
2023-05-15 13:51:16 +0900

    1에서 2로 수정

--- polls/views.py
+++ polls/views.py
@@ -6,7 +6,7 @@ from polls.models import Question, Choice
 
 def index(request):
     latest_question_list = Question.objects.all().order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
+    context = {'question_list': latest_question_list}
 
     return render(request, 'polls/index.html', context)
 
